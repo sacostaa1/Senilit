977c541a07e1096d32984e5abba91a0d90ccfa87
hdezdav <157717974+hdezdav@users.noreply.github.com>
2025-02-10 10:05:48 -0500

    CSS y Estetica
    Mejoras estéticas

--- login/settings.py
+++ login/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Asegúrate de que tus archivos estáticos estén en la carpeta 'static' dentro de tu proyecto
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
