5a46725ed5a3d6675e6774fa7eefac0c7ceaedbf
Samuel Acosta <sacostaa1@eafit.edu.co>
2025-05-16 07:46:03 -0500

    Update urls.py

--- senelit/myapp/urls.py
+++ senelit/myapp/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from myapp.views import signup_view
 
 urlpatterns = [
     path('service/create/', views.create_service, name='create_service'),
@@ -8,6 +10,7 @@ urlpatterns = [
     path('services/', views.service_list, name='service_list'),
     path('toggle-favorite/<int:service_id>/', views.toggle_favorite, name='toggle_favorite'),
     path('favorites/', views.favorite_list, name='favorite_list'),
- 
-
+    path('signup/', signup_view, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
